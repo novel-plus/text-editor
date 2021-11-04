5bdffbc08456e341b62b5b9ef026beb37741d071
fifteenmania <ktthee1995@gmail.com>
2021-11-04 16:45:59 +0900

    feat: implement save

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
-const { app, BrowserWindow, ipcMain, dialog, Menu, MenuItem } = require('electron');
+const { app, BrowserWindow, ipcMain, dialog, Menu, MenuItem, globalShortcut } = require('electron');
 const path = require('path');
-const fs = require('fs');
+const { promises } = require('fs');
+
+const indexHtmlPath = 'editor/index.html';
+const preloadJsPath = 'editor/preload.js';
 
 function createMainWindow() {
     const mainWindow = new BrowserWindow({
+        width: 1000,
+        height: 1000,
         webPreferences: {
-            preload: path.join(__dirname, 'preload.js')
+            preload: path.join(__dirname, preloadJsPath)
         },
         show: false
     })
-    mainWindow.maximize();
     const menu = createMenu()
     Menu.setApplicationMenu(menu);
-    mainWindow.loadFile('editor/index.html');
+    mainWindow.loadFile(indexHtmlPath);
+
+    mainWindow.webContents.openDevTools();
     return mainWindow;
 }
 
@@ -22,11 +28,11 @@ function createWindow() {
         x: focusedWindow.x + 10,
         y: focusedWindow.y + 10,
         webPreferences: {
-            preload: path.join(__dirname, 'preload.js')
+            preload: path.join(__dirname, preloadJsPath)
         },
         show: false
     });
-    newWindow.loadFile('editor/index.html');
+    newWindow.loadFile(indexHtmlPath);
     newWindow.once('ready-to-show', () => {
         newWindow.show();
     })
@@ -47,10 +53,51 @@ function createMenu() {
             role: 'load file',
             label: '불러오기',
             accelerator: isMac ? 'Cmd+O' : 'Ctrl+O',
-            click: () => loadTextFile(BrowserWindow.getFocusedWindow())
+            click: () => {
+                const focusedWindow = BrowserWindow.getFocusedWindow();
+                loadTextFile(focusedWindow)
+                .then((result)=>focusedWindow.webContents.send('file-render:opened', result))
+            }
+        }, {
+            role: 'save file',
+            label: '저장',
+            accelerator: isMac ? 'Cmd+S' : 'Ctrl+S',
+            click: () => {
+                const focusedWindow = BrowserWindow.getFocusedWindow();
+                focusedWindow.webContents.send('file-render:save-request')
+            }
         }]
     }))
     return menu;
+}
+
+async function asyncReadFile(file) {
+    const result = {
+        canceled: true,
+        path: '',
+        content: ''
+    }
+    const canceled = file.canceled;
+    const path = file.filePaths[0];
+    if (canceled || !path)
+        return result;
+    const content = await promises.readFile(path, {encoding: 'utf-8'});
+
+    result.canceled = canceled;
+    result.path = path;
+    result.content = content;
+    return result;
+}
+
+async function asyncWriteFile(file, content) {
+    if (!!file.canceled) return;
+    const filename = file.filePath;
+    try {
+        await promises.writeFile(filename, content, {encoding: 'utf-8'});
+        return true;
+    } catch (error) {
+        return false;
+    }
 }
 
 async function loadTextFile(targetWindow) {
@@ -62,32 +109,35 @@ async function loadTextFile(targetWindow) {
             { name: '모든 파일', extensions: ['*'] }
         ]
     });
-    const result = {
-        canceled: true,
-        path: '',
-        content: ''
-    }
-    const canceled = file.canceled;
-    const path = file.filePaths[0];
-    if (canceled || !path)
-        return result;
-    const content = fs.readFileSync(path).toString();
+    const result = await asyncReadFile(file);
+    return result;
+}
 
-    result.canceled = canceled;
-    result.path = path;
-    result.content = content;
+async function saveTextFile(targetWindow, filename, content) {
+    const file = await dialog.showSaveDialog(targetWindow, {
+        defaultPath: filename,
+        filters: [
+            { name: '마크다운 파일', extensions: ['md', 'markdown'] },
+            { name: '텍스트 파일', extensions: ['txt'] },
+            { name: '모든 파일', extensions: ['*'] }
+        ]
+    });
+    const result = await asyncWriteFile(file, content);
     return result;
 }
 
 function initializeMain() {
     const mainWindow = createMainWindow();
     
-    ipcMain.handle('fileControl:open', (event) => {
+    ipcMain.handle('file-control:open', (event) => {
         return loadTextFile(mainWindow);
     })
-    ipcMain.handle('fileControl:new', (event) => {
+    ipcMain.handle('file-control:new', (event) => {
         const newWindow = createWindow();
         return true;
+    })
+    ipcMain.handle('file-control:save', (event, filename, content) => {
+        return saveTextFile(mainWindow, filename, content);
     })
     mainWindow.once('ready-to-show', () => {
         mainWindow.show();
